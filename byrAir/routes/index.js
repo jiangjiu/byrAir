9be9ee8603033a8224a5ab316e812eb9b7e63446
zhangsiyuan <10821808625@qq.com>
2015-07-21 22:25:50 +0800

    marked 嵌入 简单实现

--- byrAir/routes/index.js
+++ byrAir/routes/index.js
@@ -1,5 +1,14 @@
 module.exports = function(app) {
     app.get('/', function (req, res) {
-        res.render('index', { title: 'Express' });
+        var data = [];
+        var article = {
+            type:'设计',
+            title: '图片排版与字体排版',
+            content: '# Marked in browser\n\nRendered by **marked**.'
+        };
+        for(var i = 0;i<20;i++) {
+            data.push(article);
+        }
+        res.render('index', { data: data });
     });
 };
